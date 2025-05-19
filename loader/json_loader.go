a90a50c1dab11bc8d59a2efbeec3e278aa6d5b5b
mrcong <zh-lei@foxmail.com>
2025-05-19 17:05:37 +0800

    fix json loader

--- loader/json_loader.go
+++ loader/json_loader.go
@@ -1,10 +1,25 @@
 package loader
 
-import "github.com/zoneBen/ProtoHub/modu"
+import (
+	"encoding/json"
+	"errors"
+	"github.com/zoneBen/ProtoHub/modu"
+	"io"
+	"os"
+)
 
 type JsonLoader struct {
 }
 
-func (j *JsonLoader) Load(path string) (modu.EParser, error) {
-	return modu.EParser{}, nil
+func (j *JsonLoader) Load(filePath string) (modu.EParser, error) {
+	var dev modu.EParser
+	f, err := os.Open(filePath)
+	defer f.Close()
+	if err != nil {
+		err = errors.New("Load file error")
+		return dev, err
+	}
+	data, err := io.ReadAll(f)
+	err = json.Unmarshal(data, &dev)
+	return dev, err
 }
